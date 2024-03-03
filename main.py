5362771be78e75be6cd44db5a7076f59f60f7e4e
徐欧晓 <156639497+xuouxiao1024@users.noreply.github.com>
2024-03-03 10:07:07 +0800

    main

--- main.py
+++ main.py
@@ -6,7 +6,16 @@ music.play('taiji')
 fupan=Actor('复盘',(100,100))
 renji=Actor('人机',(100,250))
 twopeo=Actor('双人',(100,400))
-f,r,t=False,False,False
+qizi=[Actor('红车',(80,55)),Actor('红马',(160,55)),Actor('红相',(240,55)),Actor('红士',(320,55)),Actor('红帅',(400,55)), Actor('红士',(480,55)),
+      Actor('红相',(560,55)),Actor('红马',(640,55)),Actor('红车',(720,55)),Actor('红炮',(160,205)), Actor('红炮',(640,205)), Actor('红兵',(80,280)),
+      Actor('红兵',(240,280)),Actor('红兵',(400,280)), Actor('红兵',(560,280)), Actor('红兵',(720,280)),
+      Actor('卒',(80,530)), Actor('卒',(240,530)), Actor('卒',(400,530)), Actor('卒',(560,530)), Actor('卒',(720,530)),
+      Actor('炮',(160,610)), Actor('炮',(640,610)),Actor('车', (80, 770)), Actor('馬', (160, 770)), 
+      Actor('象', (240, 770)),Actor('士', (320, 770)), Actor('将', (400, 770)), Actor('士', (480, 770)), 
+      Actor('象', (560, 770)),Actor('馬', (640, 770)), Actor('车', (720, 770))
+      ]
+f, r, t = False, False, False
+zt=0
 def update_time():
     global time 
     time+=0.1
@@ -29,9 +38,17 @@ def draw():
         fupan.draw()
         renji.draw()
         twopeo.draw()
-        screen.draw.text("象棋，亦作中国象棋，中国传统棋类\n益智游戏，在中国有着悠久的历史，属于二人\n对抗性游戏的一种，由于用具简单，趣味性强，\n成为流行极为广泛的棋艺活动。 中国象棋是\n中国棋文化也是中华民族的文化瑰宝。", (250, 250),fontname="华文行楷", fontsize=32, color='black')
-    if t==True:  # 双人模式
+        screen.draw.text("象棋，亦作中国象棋，中国传统棋类\n\
+益智游戏，在中国有着悠久的历史，\n\
+属于二人对抗性游戏的一种，\n\
+由于用具简单，趣味性强，\n\
+成为流行极为广泛的棋艺活动。\n\
+中国象棋是中国棋文化\n\
+也是中华民族的文化瑰宝。", (200, 300),fontname="华文行楷", fontsize=24, color='black')
+    if t == True:  # 双人模式
         screen.blit('qp', (0, 0))
+        for i in qizi:
+            i.draw()
 def update():
     pass
 go()
